2d3639eddec51ad29e8d8570ce14aee0258b975c
Daniel Reinecke <danielluxio3@gmail.com>
2024-11-14 17:21:22 -0500

    Updating speech and listening

--- detection.py
+++ detection.py
@@ -7,5 +7,15 @@ def getSpeech():
     while True:
         try:
             with sr.Microphone() as mic:
+                #remove abient noise from the audio
                 r.adjust_for_ambient_noise(mic, duration = 0.2)
-        except:
+                #get audio input
+                audio = r.listen(mic)
+                #convert audio to String
+                text = r.recognize_google(audio)
+                text = text.lower()
+                return text
+        #make sure the program keeps looping if bad audio is given
+        except sr.UnknownValueError:
+            r = sr.Recognizer()
+            continue
--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from Assistant2 import Assistant, Language_Model
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
+import detection as dc
+import Speech as s
 
 # main loop
 while True:
-    # Listen for audio
-
-    # audio -> text
-
+    # Listen for audio and convert to text
+    text = dc.getSpeech()
     # send text to the assistant
 
     # get response from the assistant
 
     # response -> audio
-
+    s.speak(text)
     # play audio
